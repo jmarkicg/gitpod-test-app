0641c8dfd8ff30970a1becc2e64b698b34243d85
jmarkicg <jelena.markic@degloo.com>
2020-05-18 13:19:26 +0200

    Added getters/setters

--- src/main/java/hr/markic/dto/UserDTO.java
+++ src/main/java/hr/markic/dto/UserDTO.java
@@ -21,4 +21,36 @@ public class UserDTO {
     public void setId(Long id) {
         this.id = id;
     }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 }
